22f26f26f9452b264e2f7f08f132104d2f76b2c6
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-09 08:31:54 +0300

    скелет проекта создан

---
+++ Api/Controllers/TextSourceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextSourceController : ControllerBase
+    {
+        public TextSourceController()
+        {
+            
+        }
+
+        [HttpPost]
+        public void Search()
+        {
+        }
+    }
+}
