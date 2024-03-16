7f5275fac6aba0d2a6476ce0b07578e38d217e7b
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-16 14:55:42 +0300

    Client: добавлена ссылка на открытие страницы

--- Frontend/Client/src/app/app-routing.module.ts
+++ Frontend/Client/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./security/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./text-source/text-source.module').then(m => m.TextSourceModule)
   }
 ];
 
