20a4f38a97a7d5585a8895cfc00e00c1431104f0
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-20 18:13:55 +0300

    SqlClr.Assemblies : создан проект и добавлен класс RegularExpressions с методом SqlClrRegexMatch

---
+++ Backend/SqlClr.Assemblies/RegularExpressions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+
+namespace SqlClr.Assemblies
+{
+    public partial class RegularExpressions
+    {
+        [SqlFunction(IsDeterministic = true)]
+        public static SqlBoolean SqlClrRegexMatch(SqlChars input, SqlString pattern)
+        {
+            if (input.IsNull || pattern.IsNull)
+                return SqlBoolean.Null;
+
+            Regex regex = new Regex(pattern.Value);
+            return regex.IsMatch(new string(input.Value));
+        }
+    }
+}
